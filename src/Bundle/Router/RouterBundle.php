23e5a0888ab175f3b986ca7d1efa3b9c607dcf13
Dmitrij Abramov <dimabradima@gmail.com>
2024-01-06 02:28:11 +0300

    Дописал RouterBundle

--- src/Bundle/Router/RouterBundle.php
+++ src/Bundle/Router/RouterBundle.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Kaa\Bundle\Router;
 
 use Kaa\Bundle\Framework\BundleGeneratorInterface;
+use Kaa\Component\GeneratorContract\PhpOnly;
 use Kaa\Component\GeneratorContract\SharedConfig;
 use Kaa\Component\Router\RouterGenerator;
 use Kaa\Component\Router\RouterInterface;
 use Kaa\HttpKernel\HttpKernelEvents;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
+#[PhpOnly]
 readonly class RouterBundle extends RouterGenerator implements BundleGeneratorInterface
 {
     public function generate(SharedConfig $sharedConfig, $config): void
